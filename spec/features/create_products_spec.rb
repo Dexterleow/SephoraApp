829d28f6c30775b1fbd3143c8e29711620cdd65b
dexterleow <gdleow@gmail.com>
2017-09-03 12:40:58 +0800

    Define tests for creation of product

---
+++ spec/features/create_products_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper.rb'
+
+# visit root route
+# click create product link
+# fill in the form with needed information (name, description, price in SGD)
+# click submit button
+# expect page to have the content we submitted
