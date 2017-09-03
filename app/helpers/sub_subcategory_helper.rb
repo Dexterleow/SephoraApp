ead9cb96d7d887cafe7d3ee7ae40d286473aacd4
dexterleow <gdleow@gmail.com>
2017-09-03 20:16:33 +0800

    Generate sub_subcategory model

---
+++ app/helpers/sub_subcategory_helper.rb
@@ -0,0 +1,2 @@
+module SubSubcategoryHelper
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903050530) do
+ActiveRecord::Schema.define(version: 20170903120531) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20170903050530) do
     t.decimal  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "sub_subcategories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
