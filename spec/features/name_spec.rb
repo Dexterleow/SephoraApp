e72d36dd3d47ac445687653888fee71ba056b853
dexterleow <gdleow@gmail.com>
2017-09-03 17:00:56 +0800

    Able to pass all tests again

--- spec/features/name_spec.rb
+++ spec/features/name_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper.rb'
 feature "visiting the homepage" do
     scenario "index page show welcome text" do 
         visit root_path
-        expect(page).to have_text("Welcome to Products")
+        expect(page).to have_text("Welcome to Sephora")
     end
 end
