575bd6da04ff72dc53a6e033f2f9d1c133743982
dexterleow <gdleow@gmail.com>
2017-09-03 12:50:49 +0800

    write out specific rspec tests

--- spec/features/create_products_spec.rb
+++ spec/features/create_products_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper.rb'
 
-# visit root route
-# click create product link
-# fill in the form with needed information (name, description, price in SGD)
-# click submit button
-# expect page to have the content we submitted
+feature 'Creating Products' do 
+    scenario 'can create product' do
+        # visit root route
+        visit '/'
+        # click create product link
+        click_link 'Create Product'
+        # fill in the form with needed information (name, description, price in SGD)
+        fill_in 'Name', with: 'name'
+        fill_in 'Description', with: 'description'
+        fill_in 'Price', with: 'price'
+        # click submit button
+        click_button 'Create Update'
+        # expect page to have the content we submitted
+        expect(page).to have_content('name')
+        expect(page).to have_content('description')
+        expect(page).to have_content('price')
+    end
+end
